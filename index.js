4d67db715b5ff4d242ddc3348212dd9800e65e34
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-10-26 19:31:13 -0300

    Agregado prueba socket io

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ inicializar_1.initApi(app);
 var server = require('http').Server(app);
 var port = 3002;
 server = app.listen(3002, function () { return console.log("Escuchando en el puerto %s ", port); });
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
+io.emit('test', 'enviando desde backend');
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { initApi } from './inicializar';
+import * as io from 'socket.io';
 
 const app = express();
 
@@ -9,3 +10,13 @@ var server = require('http').Server(app);
 
 const port = 3002;
 server = app.listen(3002, () => console.log("Escuchando en el puerto %s ", port));
+
+var io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    
+  });
+io.emit('test','enviando desde backend');
+
+
